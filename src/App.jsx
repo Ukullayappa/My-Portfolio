210735f2e3a6d40c237ff677dcde48ee3f2b5957
Ukullayappa <ukullayappa1@gmail.com>
2026-04-21 13:29:51 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
@@ -8,6 +9,7 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Admin from './Admin'
+import Login from './Login'
 
 function Home() {
   return (
@@ -25,13 +27,21 @@ function Home() {
   )
 }
 
+function AdminProtected() {
+  const isAuth = localStorage.getItem('admin_auth') === 'true'
+
+  if (!isAuth) {
+    return <Login onLogin={() => window.location.reload()} />
+  }
+
+  return <Admin />
+}
+
 export default function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/admin" element={<AdminProtected />} />
+    </Routes>
   )
 }
